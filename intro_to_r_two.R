615e3d37c01d4b26f8a483c251d8315ec0cc164e
Mauricio M. M. <MauricioMM7@outlook.com>
2024-09-01 21:31:03 +0200

    Created file for workshop

---
+++ intro_to_r_two.R
@@ -0,0 +1,28 @@
+######################################
+# IUROPA Workshop 2024
+# 2024 September 19-23
+######################################
+
+# Setting your work environment ---- 
+s <-  new 
+######################
+# CHECK Session info 
+sessionInfo()
+# FIND Where R executable lives
+file.path(R.home("bin"), "R")
+getwd()
+#CHECK if you have installed package
+a<-installed.packages()
+packages<-a[,1] 
+is.element("boot", packages)
+
+
+# Loading the dataset  ----
+
+
+
+# CREATE new variable using  sing case_when
+
+# CREATE some basic visuzalization
+
+#
