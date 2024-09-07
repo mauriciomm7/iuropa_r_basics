4d16a4f74ac1cc29aaed466199c0dcf64eb23604
Mauricio M. M. <MauricioMM7@outlook.com>
2024-09-08 00:03:14 +0200

    added slides and Rscript outline

--- intro_to_r_two.R
+++ intro_to_r_two.R
@@ -1,28 +1,41 @@
-######################################
+####################################
 # IUROPA Workshop 2024
-# 2024 September 19-23
-######################################
+# 2024 September 19th-23th
+# Author: Mauricio M. M
+####################################
 
-# Setting your work environment ---- 
-s <-  new 
-######################
-# CHECK Session info 
+####################################
+# Setting your work environment ----
+####################################
+s <-  new
+# CHECK Session
 sessionInfo()
 # FIND Where R executable lives
 file.path(R.home("bin"), "R")
 getwd()
 #CHECK if you have installed package
-a<-installed.packages()
-packages<-a[,1] 
+a <- installed.packages()
+packages <- a[, 1]
 is.element("boot", packages)
 
 
-# Loading the dataset  ----
+####################################
+# LOADING the datasets          ----
+
+
+# WORKING with lists  			----
+
+
+####################################
+# VARIABLE creation 			----
+
+# RENAME existing
+
+
+# CREATE new variable using sing case_when
 
 
 
-# CREATE new variable using  sing case_when
 
+####################################
 # CREATE some basic visuzalization
-
-#
